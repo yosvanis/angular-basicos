e831f3f8c1a3af81064dff06b1cbe0c8db4cd0c2
Yosvanis Posada García <yosvanis.posada@gmail.com>
2023-09-30 14:43:50 -0500

    segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,22 @@ import { AppComponent } from './app.component';
  
 import { HeroesModule } from './heroes/heroes.module';
 import { ContadorModule } from './contador/contador.module';
+import { MainPageComponent } from './dbz/main-page/main-page.component';
+import { DbzModule } from './dbz/dbz.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+   
          
   ],
   imports: [
     BrowserModule,
     HeroesModule,
-    ContadorModule
+    ContadorModule,
+    DbzModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
